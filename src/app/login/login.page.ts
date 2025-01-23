858dd808dd130f7bdd46bac7d960b058b6c89d14
Giovanni <20233L001003@utcv.edu.mx>
2025-01-23 09:00:04 -0600

    Implementar formulario

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  email: string = 'giogarrey@gmail..com';
-  password: string = '';
+  email: string = 'admin';
+  password: string = 'admin';
 
   constructor() {}
 
   ngOnInit() {
   }
   
-  login() {
+  login(form : NgForm) {
     // Si usuario=admijn y password=admin
     //Entonces console.log('Login Correcto')
     //Si no console.log('Login Incorrecto')
+    
+    
+    //Obtener los valores del formulario
+    console.log(form.value);
+
+    //Validar formulario
+    console.log('valid:', form.valid);
+
+    //Si form es inválido
+    //console.log ('Todos los campos son requeridos')
+    //return
+
+    if(form.invalid){
+    console.log ('Todos los campos son requeridos');
+    return;
+    }
     if(this.email === 'admin'
     && this.password === 'admin'){
       console.log('Login Correcto');
